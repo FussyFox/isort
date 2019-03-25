abfc218f7b40c0922f5cfe99ebafad037964e4d2
Johannes Hoppe <info@johanneshoppe.com>
2019-03-25 14:29:48 +0100

    Add inital isort function

---
+++ main.py
@@ -0,0 +1,14 @@
+"""Lambda function that executes isort, a static file linter."""
+import logging
+import sys
+
+from lintipy import CheckRun
+
+root_logger = logging.getLogger('')
+root_logger.setLevel(logging.DEBUG)
+root_logger.addHandler(logging.StreamHandler(sys.stdout))
+
+handle = CheckRun.as_handler(
+    'isort',
+    'isort', '--check-only', '--recursive', '.'
+)
