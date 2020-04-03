819a294d3320768fba85b95f4f6a994ad79d21cf
Johannes Hoppe <info@johanneshoppe.com>
2020-04-03 16:09:17 +0200

    Remove explicit logger setup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 """Lambda function that executes isort, a static file linter."""
-import logging
-import sys
-
 from lintipy import CheckRun
 
-root_logger = logging.getLogger('')
-root_logger.setLevel(logging.DEBUG)
-root_logger.addHandler(logging.StreamHandler(sys.stdout))
 
 handle = CheckRun.as_handler(
     'isort',
